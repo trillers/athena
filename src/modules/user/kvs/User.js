6b5fbcad21725adbee085a5096baba647e07f4fe
node-rookie <749924911@qq.com>
2015-08-11 16:04:06 +0800

    add user module

---
+++ src/modules/user/kvs/User.js
@@ -0,0 +1,207 @@
+var redis = require('../app/redis');
+var logger = require('../app/logging').logger;
+var _ = require('underscore');
+var Promise = require('bluebird');
+var cbUtil = require('../framework/callback');
+
+var idToUserKey = function(id){
+    return 'usr:id:' + id;
+};
+
+var tokenToIdKey = function(token){
+    return 'usr:tk:' + token;
+};
+
+var openidToIdKey = function(openid){
+    return 'usr:oid:' + openid;
+};
+
+var openidToAtKey = function(openid){
+    return 'usr:oid:at' + openid;
+};
+
+var idToFlagsKey = function(id){
+    return 'usr:flags:' + id;
+};
+
+var openidToFlagsKey = function(openid){
+    return 'usr:oid:flags:' + openid;
+};
+
+var processUserToSave = function(sourceObject){
+    var targetObject = _.clone(sourceObject);
+    targetObject.crtOn && (delete targetObject.crtOn);
+    targetObject.wx_subscribe_time && (delete targetObject.wx_subscribe_time);
+    targetObject.contact && (delete targetObject.contact);
+
+    targetObject.latestLatitude && (delete targetObject.latestLatitude);
+    targetObject.latestLongitude && (delete targetObject.latestLongitude);
+    targetObject.latestPrecision && (delete targetObject.latestPrecision);
+    targetObject.latestLocationTime && (delete targetObject.latestLocationTime);
+    targetObject.latestLocationCity && (delete targetObject.latestLocationCity);
+    targetObject.latestLocationAddress && (delete targetObject.latestLocationAddress);
+
+    return targetObject;
+};
+
+var User = {
+
+    loadIdByToken: function(token, callback){
+        var key = tokenToIdKey(token);
+        redis.get(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to get user id by token ' + token + ': ' + err,
+                'Succeed to get user id ' + result + ' by token ' + token);
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    linkToken: function(token, id, callback){
+        var key = tokenToIdKey(token);
+        redis.set(key, id, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to link token ' + token + ' to id ' + id + ': ' + err,
+                'Succeed to link token ' + token + ' to id ' + id);
+            cbUtil.handleOk(callback, err, result);
+        });
+    },
+
+    unlinkToken: function(token, callback){
+        var key = tokenToIdKey(token);
+        redis.del(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to delete user id by token ' + token + ': ' + err,
+                'Succeed to delete user id by token ' + token);
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    loadIdByOpenid: function(openid, callback){
+        var key = openidToIdKey(openid);
+        redis.get(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to get user id by openid ' + openid + ': ' + err,
+                'Succeed to get user id ' + result + ' by openid ' + openid);
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    linkOpenid: function(openid, id, callback){
+        var key = openidToIdKey(openid);
+        redis.set(key, id, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to link openid ' + openid + ' to id ' + id + ': ' + err,
+                'Succeed to link openid ' + openid + ' to id ' + id);
+            cbUtil.handleOk(callback, err, result);
+        });
+    },
+
+    unlinkOpenid: function(openid, callback){
+        var key = openidToIdKey(openid);
+        redis.del(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to delete user id by openid ' + openid + ': ' + err,
+                'Succeed to delete user id by openid ' + openid);
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    loadById: function(id, callback){
+        var key = idToUserKey(id);
+        redis.hgetall(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to get user by id ' + id + ': ' + err,
+                'Succeed to get user by id ' + id);
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    saveById: function(user, callback){
+        var key = idToUserKey(user.id);
+        var userToSave = processUserToSave(user);
+        redis.hmset(key, userToSave, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to save user by id ' + user.id + ': ' + err,
+                'Succeed to save user by id ' + user.id);
+            cbUtil.handleOk(callback, err, result, userToSave);
+        });
+    },
+
+    deleteById: function(id, callback){
+        var key = idToUserKey(id);
+        redis.del(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to delete user by id ' + id + ': ' + err,
+                'Succeed to delete user by id ' + id);
+
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    },
+
+    getAccessToken: function(openid, callback){
+        var key = openidToAtKey(openid);
+        redis.get(key, function(err, result){
+            cbUtil.handleOk(callback, err, result, result);
+        });
+    },
+
+    saveAccessToken: function(openid, accessToken, callback){
+        var key = openidToAtKey(openid);
+        redis.set(key, accessToken, function(err, result){
+            cbUtil.handleOk(callback, err, result);
+        });
+    },
+
+    setFlagsByOpenid: function(openid, flags, callback){
+        var key = openidToFlagsKey(openid);
+        redis.hmset(key, flags, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to set user flags by openid ' + openid + ': ' + err,
+                'Succeed to set user flags by openid ' + openid);
+            cbUtil.handleOk(callback, err, result, flags);
+        });
+    },
+
+    getFlagsByOpenid: function(openid, callback){
+        var key = openidToFlagsKey(openid);
+        redis.hgetall(key, function(err, result){
+            cbUtil.logCallback(
+                err,
+                'Fail to get user flags by openid ' + openid + ': ' + err,
+                'Succeed to get user flags by openid ' + openid);
+            cbUtil.handleSingleValue(callback, err, result);
+        });
+    }
+
+};
+
+User.setFlagResignin = function(openid, resignin, callback){
+    var flags = {'resignin': resignin ? 'T' : 'F'};
+    User.setFlagsByOpenid(openid, flags, function(err){
+        if(callback) callback(err);
+    });
+};
+
+User.getFlagResignin = function(openid, callback){
+    User.getFlagsByOpenid(openid, function(err, flags){
+        if(callback) callback(err, flags && flags.resignin === 'T');
+    });
+};
+
+User = Promise.promisifyAll(User);
+
+User.flags = {
+    resignin: 'resignin' //"T": true, "F": false
+};
+
+module.exports = User;
---
+++ src/modules/user/services/WechatUserService.js
@@ -0,0 +1,233 @@
+var User = require('../models/User').model;
+var UserHelper = require('../models/User').helper;
+var UserState = require('../framework/model/enums').UserState;
+var UserKv = require('../kvs/User');
+var time = require('../app/time');
+var settings = require('mit-settings');
+var logger = require('../app/logging').logger;
+var u = require('../app/util');
+var wechat = require('../app/wechat/api');
+var Service = {};
+var Promise = require('bluebird');
+var cbUtil = require('../framework/callback');
+
+var generateUserToken = function(uid){
+    var key = settings.secretKey;
+    return require('crypto').createHash('sha1').update(String(uid)).update(key).digest('hex');
+};
+
+Promise.promisifyAll(User);
+var createUser = function (userInfo, callback) {
+    var user = new User(userInfo);
+    var uid = user.autoId();
+    user.token = generateUserToken(uid); //TODO: use token generator
+    user.save(function (err, result, affected) {
+        //TODO: logging
+        cbUtil.handleAffected(callback, err, result, affected);
+    });
+};
+var createUserAsync = Promise.promisify(createUser);
+var updateUser = function (id, update, callback) {
+    User.update({_id: id}, update, function (err, doc) {
+        if (err) {
+            logger.error('Fail to update user [id=' + id + ']: ' + err);
+            if (callback) callback(err);
+        }
+        else{
+            if (callback) callback(null, doc);
+        }
+    });
+};
+var updateUserAsync = Promise.promisify(updateUser);
+
+var loadById = function(id, callback){
+    User.findById(id).exec(function(err, doc){
+        if (err) {
+            logger.error('Fail to load user [id=' + id + ']: ' + err);
+            if(callback) callback(err);
+        }
+        else{
+            logger.debug('Succeed to load user [id=' + id + ']');
+            if(callback) callback(null, doc);
+        }
+    });
+};
+var loadByIdAsync = Promise.promisify(loadById);
+
+/**
+ *  Create a registered user from wechat, not an anonymous user
+ * @param user user json object
+ * @param callback
+ */
+Service.createFromWechat = function (userInfo, callback) {
+    if(userInfo.wx_nickname){
+        userInfo.stt = UserState.Registered;
+    }
+    else{
+        userInfo.stt = UserState.Registered; //UserState.Registered;
+    }
+    return createUserAsync(userInfo)
+        .then(function (user) {
+            return UserHelper.getUserJsonFromModel(user);
+        })
+        .then(function (userJson) {
+            return UserKv.saveByIdAsync(userJson);
+        })
+        .then(function (userJson) {
+            return UserKv.linkTokenAsync(userJson.token, userJson.id)
+                .then(function () {
+                    return userJson;
+                });
+        })
+        .then(function (userJson) {
+            return UserKv.linkOpenidAsync(userJson.wx_openid, userJson.id)
+                .then(function () {
+                    return userJson;
+                });
+        })
+        .then(function (userJson) {
+            if(callback) callback(null, userJson);
+            return userJson;
+        })
+        .catch(Error, function (err) {
+            logger.error('Fail to create user from wechat: ' + err);
+            if(callback) callback(err);
+        });
+};
+
+Service.updateFromWechat = function(id, update, callback){
+    var newUserJson = null;
+    var toUpdate = {};
+
+    /*
+     * Update stt properties
+     */
+    if(update.wx_nickname){
+        toUpdate.stt = UserState.Registered;
+    }
+    else{
+        toUpdate.stt = UserState.Registered; //UserState.Anonymous;
+    }
+
+    /*
+     * Remove undefined properties voiding update errors
+     */
+    for (var prop in update) {
+        if(typeof update[prop] !== 'undefined'){
+            toUpdate[prop] = update[prop];
+        }
+    }
+
+    return updateUserAsync(id, toUpdate)
+        .then(function(){
+            return loadByIdAsync(id);
+        })
+        .then(function(user){
+            return newUserJson = UserHelper.getUserJsonFromModel(user);
+        })
+        .then(function(userJson){
+            return UserKv.saveByIdAsync(userJson);
+        })
+        .then(function(){
+            return UserKv.linkTokenAsync(newUserJson.token, newUserJson.id);
+        })
+        .then(function () {
+            if(callback) callback(null, newUserJson);
+            return newUserJson;
+        })
+        .catch(Error, function(err){
+            logger.error('Fail to update user from wechat: ' + err);
+            if(callback) callback(err);
+        });
+};
+
+var getUserFromWechat = function(openid, callback){
+    wechat.api.getUser(openid, function(err, userInfo){
+        if(err){
+            if(callback) callback(err);
+        }
+        else{
+            if(callback) callback(null, userInfo);
+        }
+    });
+};
+Service.getUserFromWechat = getUserFromWechat;
+
+var getUserFromWechatAsync = Promise.promisify(getUserFromWechat);
+Service.getUserFromWechatAsync = getUserFromWechatAsync;
+
+Service.loadOrCreateFromWechat = function(openid, callback){
+    return UserKv.loadIdByOpenidAsync(openid)
+        .then(function(id){
+            return id && UserKv.loadByIdAsync(id);
+        })
+        .then(function(user){
+            if (user) return user;
+            return getUserFromWechatAsync(openid)
+                .then(function (userInfo) {
+                    return UserHelper.getUserJsonFromWechatApi(userInfo)
+                })
+                .then(Service.createFromWechat);
+        })
+        .then(function(user){
+            if(callback) callback(null, user);
+            return user;
+        })
+        .catch(Error, function (err) {
+            logger.error('Fail to load or create user by openid: ' + err);
+            if(callback) callback(err);
+        });
+};
+
+Service.deleteByOpenid = function(openid, callback){
+    return UserKv.loadIdByOpenidAsync(openid)
+        .then(function(id){
+            return id && UserKv.loadByIdAsync(id);
+        })
+        .then(function(user){
+            if (user) return user;
+            return getUserFromWechatAsync(openid)
+                .then(function (userInfo) {
+                    return UserHelper.getUserJsonFromWechatApi(userInfo);
+                })
+                .then(Service.createFromWechat);
+        })
+        .then(function(user){
+            if(callback) callback(null, user);
+            return user;
+        })
+        .catch(Error, function (err) {
+            logger.error('Fail to delete user by openid: ' + err);
+            if(callback) callback(err);
+        });
+};
+
+Service.createOrUpdateFromWechatOAuth = function(oauth, callback){
+    var newUserJson = null;
+    var openid = oauth.openid;
+    return getUserFromWechatAsync(openid)
+        .then(function (userJson) {
+            return newUserJson = UserHelper.mergeUserInfo(oauth, userJson);
+        })
+        .then(function(userJson){
+            return UserKv.loadIdByOpenidAsync(openid);
+        })
+        .then(function(id){
+            if(id){
+                return Service.updateFromWechat(id, newUserJson);
+            }
+            else{
+                return Service.createFromWechat(newUserJson);
+            }
+        })
+        .then(function(userJson){
+            if(callback) callback(null, userJson);
+            return userJson;
+        })
+        .catch(Error, function (err) {
+            logger.error('Fail to create or update user by openid: ' + err);
+            if(callback) callback(err);
+        });
+};
+
+module.exports = Service;
