954b91f0af0ad0cc1ccb53189067400f2890b59e
node-rookie <749924911@qq.com>
2015-08-12 11:50:22 +0800

    modify qrchannel handler

--- src/modules/qrchannel/handlers/CustomerServerHandler.js
+++ src/modules/qrchannel/handlers/CustomerServerHandler.js
@@ -6,19 +6,22 @@ var UserRole = require('../../common/models/TypeRegistry').item('UserRole');
 
 var handle = function(message, user, res, qrChannel){
     //TODO: implementation
-    var update = {
+    var userUpdate = {
         wx_subscribe: 1,
         wx_subscribe_time: new Date(),
         $inc: {'subscribeCount': 1},
+    };
+
+    var userBizUpdate = {
         role: UserRole.RegularUser.value()
     };
 
-    UserService.updateAsync(user.id, update)
-        .then(function(result){
-            return ClazzTeacherService.createAsync({user: user.id, headUrl: user.wx_headimgurl, name: user.wx_nickname});
+    UserService.updateAsync(user.id, userUpdate)
+        .then(function(){
+            return UserBizService.updateByConditionAsync({user: user.id}, userBizUpdate);
         })
-        .then(function(ClazzTeacher){
-            var replyMsg = '欢迎老师注册！';
+        .then(function(){
+            var replyMsg = '欢迎注册成为客服人员！';
             res.reply(replyMsg);
         })
         .catch(Error, function(err){
@@ -26,6 +29,6 @@ var handle = function(message, user, res, qrChannel){
         });
 };
 
-var handler = new QrHandler(true, 'TS', handle); //TS teacher subscribe handler
+var handler = new QrHandler(true, 'CS', handle); //CS customer server handler
 
 module.exports = handler;
--- src/modules/qrchannel/handlers/SystemManagerHandler.js
+++ src/modules/qrchannel/handlers/SystemManagerHandler.js
@@ -1,56 +1,34 @@
 var QrHandler = require('../common/QrHandler');
-var ClazzService = require('../../../services/ClazzService');
-var ClazzBriefService = require('../../../services/ClazzBriefService');
 var UserService = require('../../user/services/UserService');
-var UserBizService = require('../../user/services/UserBizService');
-var ClazzStudentService = require('../../../services/ClazzStudentService');
-var wechatApi = require('../../../app/wechat/api').api;
-var Promise = require('bluebird');
-var UserRole = require('../../../models/TypeRegistry').item('UserRole');
-
-var tutorMediaId = "9Qx2NY4keMALv37HdZ-XfJjCsNWy1w5l3Kmar0zJc2DBtBb1fFRa_nRUv57RFv4P";
-var _replyMsg = "识别下面的二维码,可以添加您的小助手哦~";
+var UserBizService = require('../../user/services/UserBizService')
+var logger = require('../../../app/logging').logger;
+var UserRole = require('../../common/models/TypeRegistry').item('UserRole');
 
 var handle = function(message, user, res, qrChannel){
-    var update = {
-            wx_subscribe: 1,
-            wx_subscribe_time: new Date(),
-            $inc: {'subscribeCount': 1},
-            role: UserRole.Student.value()
-        },
-        clazzStudentId;
+    //TODO: implementation
+    var userUpdate = {
+        wx_subscribe: 1,
+        wx_subscribe_time: new Date(),
+        $inc: {'subscribeCount': 1},
+    };
 
-    UserService.update(user.id, update)
+    var userBizUpdate = {
+        role: UserRole.SystemManager.value()
+    };
+
+    UserService.updateAsync(user.id, userUpdate)
         .then(function(){
-            res.reply(_replyMsg);
-            return ClazzStudentService.createAsync({user: user.id, headUrl: user.wx_headimgurl, name: user.wx_nickname});
+            return UserBizService.updateByConditionAsync({user: user.id}, userBizUpdate);
         })
-        .then(function(clazzStudent){
-            clazzStudentId = clazzStudent._id;
-            return ClazzService.loadByQrChannelIdAsync(qrChannel._id);
-        })
-        .then(function(clazz){
-            return ClazzService.addStudentAsync(clazz._id, clazzStudentId, user.id);
-        })
-        .then(function(clazz){
-            return ClazzBriefService.loadByClazzIdAsync(clazz._id);
-        })
-        .then(function(clazzBrief){
-            var userBiz = {
-                $addToSet: {clazzes: clazzBrief._id}
-            }
-            return UserBizService.updateByConditionAsync({user: user.id}, userBiz);
-        })
-        .then(function(userBiz){
-            if(userBiz){
-                return wechatApi.sendImageAsync(user.wx_openid, tutorMediaId);
-            }else{
-                throw new Error("class Failed to bind Student!");
-            }
+        .then(function(){
+            var replyMsg = '欢迎注册成为系统管理员！';
+            res.reply(replyMsg);
         })
         .catch(Error, function(err){
-            console.log(err);
+            logger.error(err);
         });
 };
 
-module.exports = new QrHandler(true, 'SBC', handle);
+var handler = new QrHandler(true, 'SM', handle); //SM system manager handler
+
+module.exports = handler;
