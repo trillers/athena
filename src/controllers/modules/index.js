61b3e91c0a9773b7dad5565fc4e834757cb5d92c
node-rookie <749924911@qq.com>
2015-08-18 15:33:21 +0800

    update

--- src/controllers/modules/index.js
+++ src/controllers/modules/index.js
@@ -3,8 +3,8 @@ var router = require('koa-router');
 module.exports = function(app){
     //spa module
     var spa_router = new router();
-    require('./spa')(spa_router);
-    app.use(spa_router.routes());
+    //require('./spa')(spa_router);
+    //app.use(spa_router.routes());
 
     //wechat
     app.use(require('./wechat')());
