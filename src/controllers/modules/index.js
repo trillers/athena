440ba239066235c5b3cbbfceb4eb0a58eaad05c5
node-rookie <749924911@qq.com>
2015-08-18 15:48:16 +0800

    update

--- src/controllers/modules/index.js
+++ src/controllers/modules/index.js
@@ -4,6 +4,8 @@ module.exports = function(app){
     //spa module
     var spa_router = new router();
     require('./spa')(spa_router);
+    console.log(spa_router);
+    console.log(spa_router.routes());
     app.use(spa_router.routes());
 
     //wechat
