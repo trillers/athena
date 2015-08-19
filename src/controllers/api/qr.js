9fa57684018d69295c8b1da35fcab6b09fb942fa
node-rookie <749924911@qq.com>
2015-08-19 10:24:46 +0800

    update

--- src/controllers/api/qr.js
+++ src/controllers/api/qr.js
@@ -4,24 +4,27 @@ var logger = require('../../app/logging').logger;
 var ApiReturn = require('../../framework/ApiReturn');
 
 module.exports = function(router){
-    //getTeacher QR CODE
-    router.get('/getCSQrCode', function(req, res){
+    //get customer server QR CODE
+    router.get('/getCSQrCode', function* (){
+        var self = this;
         var key = QrChannel.genKey(true, 'CS');
         var handler = QrChannel.handlers[key];
         handler.manualCreate(50, null, function(err, qr){
             //TODO err handler
             var url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + qr.ticket;
-            res.redirect(url);
+            self.redirect(url);
         })
     });
 
-    router.get('/getSMQrCode', function(req, res){
+    //get system manager qr code
+    router.get('/getSMQrCode', function* (){
+        var self = this;
         var key = QrChannel.genKey(true, 'SM');
         var handler = QrChannel.handlers[key];
         handler.manualCreate(50, null, function(err, qr){
             //TODO err handler
             var url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + qr.ticket;
-            res.redirect(url);
+            self.redirect(url);
         })
     });
 };
