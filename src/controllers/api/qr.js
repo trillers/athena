54ce1ab433a36879aa0e69bf415593bfb749ccb1
node-rookie <749924911@qq.com>
2015-08-19 10:50:08 +0800

    update

--- src/controllers/api/qr.js
+++ src/controllers/api/qr.js
@@ -2,14 +2,15 @@ var QrChannel = require('../../modules/qrchannel');
 var util = require('util');
 var logger = require('../../app/logging').logger;
 var ApiReturn = require('../../framework/ApiReturn');
-var Promise = require('bluebird');
+var thunkify = require('thunkify');
 
 module.exports = function(router){
     //get customer server QR CODE
     router.get('/getCSQrCode', function* (){
+        var self = this;
         var key = QrChannel.genKey(true, 'CS');
         var handler = QrChannel.handlers[key];
-        var createQrCode = Promise.promisify(handler.manualCreate);
+        var createQrCode = thunkify(handler.manualCreate);
         var qr = yield createQrCode(50, null);
         var url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + qr.ticket;
         this.redirect(url);
@@ -17,9 +18,10 @@ module.exports = function(router){
 
     //get system manager qr code
     router.get('/getSMQrCode', function* (){
+        var self = this;
         var key = QrChannel.genKey(true, 'SM');
         var handler = QrChannel.handlers[key];
-        var createQrCode = Promise.promisify(handler.manualCreate);
+        var createQrCode = thunkify(handler.manualCreate);
         var qr = yield createQrCode(51, null);
         var url = 'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=' + qr.ticket;
         this.redirect(url);
