82fa71e5d1ebf5065559e4637f0829a40eda32e7
leekangtaqi <leekangtaqi@sina.com>
2015-08-20 14:50:32 +0800

    test

--- src/modules/customer_server/handlers/commands/onlineCommand.js
+++ src/modules/customer_server/handlers/commands/onlineCommand.js
@@ -1,14 +1,12 @@
 var cskv = require('../../kvs/CustomerServer');
 var wechatApi = require('../../../wechat/common/api').api;
-var conversationQueue = require('../../../conversation/common/ConversationQueue');
 module.exports = function(user, message, callback){
     cskv.saveCSStatusByCSOpenIdAsync(user.wx_openid, 'ol')
         .then(function(){
             return cskv.pushWcCSSetAsync(user.wx_openid);
         })
         .then(function(){
-            console.log('queue----------------------' + require('util').inspect(conversationQueue));
-            return conversationQueue.emit('csOnline', {csId: user.wx_openid});
+            return require('../../../conversation/common/ConversationQueue').emit('csOnline', {csId: user.wx_openid});
         })
         .then(function(){
             wechatApi.sendText(user.wx_openid, '您已上线', function(err, result){
