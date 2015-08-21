b7f54a22c041a9f696b45f2b9a177c8fb3cc1e11
leekangtaqi <leekangtaqi@sina.com>
2015-08-21 16:12:14 +0800

    test

--- src/modules/customer_server/handlers/commands/callTaxiCommand.js
+++ src/modules/customer_server/handlers/commands/callTaxiCommand.js
@@ -7,29 +7,34 @@ module.exports = function(user, message, callback){
     //save to redis
     //placeCase:openid  {type: 2ct, payload:{xxx: 1, yyy: 2}, step:2}
     co(function* (){
-        var conversation = yield cskv.loadCSSByIdAsync(user.wx_openid);
-        if(!conversation){
-            yield wechatApi.sendTextAsync(user.wx_openid, '当前没有会话');
-            return callback(null, null);
-        }
-        var bindOrNot = yield common.validateUserBindAsync(conversation.initiator);
-        if(!bindOrNot){
-            yield wechatApi.sendTextAsync(user.wx_openid, '请先绑定用户');
-            return callback(null, null);
-        }
-        var json = {
-            type: 'ct',
-            payload: {
+        try{
+            var conversation = yield cskv.loadCSSByIdAsync(user.wx_openid);
+            if(!conversation){
+                yield wechatApi.sendTextAsync(user.wx_openid, '当前没有会话');
+                return callback(null, null);
+            }
+            var bindOrNot = yield common.validateUserBindAsync(conversation.initiator);
+            if(!bindOrNot){
+                yield wechatApi.sendTextAsync(user.wx_openid, '请先绑定用户');
+                return callback(null, null);
+            }
+            var json = {
+                type: 'ct',
+                payload: {
 
-            },
-            step: 1
-        };
-        cskv.savePlaceCaseAsync(user.wx_openid, json)
-            .then(function(){
-                wechatApi.sendText(user.wx_openid, '用车时间是?', function(err, result){
-                    if(callback) return callback(err, result);
+                },
+                step: 1
+            };
+            cskv.savePlaceCaseAsync(user.wx_openid, json)
+                .then(function(){
+                    wechatApi.sendText(user.wx_openid, '用车时间是?', function(err, result){
+                        if(callback) return callback(err, result);
+                    });
                 });
-            });
+        }catch(e){
+            console.log(e)
+        }
+
     })
 
 };
