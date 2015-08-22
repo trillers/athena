c4883ea35dff9ac33a68d0938c62476cd4544d64
leekangtaqi <leekangtaqi@sina.com>
2015-08-22 17:10:13 +0800

    test

--- src/modules/customer_server/common/FSM.js
+++ src/modules/customer_server/common/FSM.js
@@ -16,7 +16,7 @@ var cmdWorkflow = FSM.create({
         {name: 'rollback', from: 'case', to: 'busy'},
         {name: 'quit', from: 'busy', to: 'free'},
         {name: 'online', from: 'off', to: 'free'},
-        {name: 'offline', from: 'free, busy', to: 'off'},
+        {name: 'offline', from: ['free', 'busy'], to: 'off'},
         {name: 'callTaxi', from: 'busy', to: 'case'}
     ]
 })
