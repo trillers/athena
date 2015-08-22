136886217b3072b31a9b1fe3ded6db99491b524a
leekangtaqi <leekangtaqi@sina.com>
2015-08-22 17:44:13 +0800

    test

--- src/modules/customer_server/handlers/commands/viewStateCommand.js
+++ src/modules/customer_server/handlers/commands/viewStateCommand.js
@@ -5,8 +5,9 @@ var co = require('co');
 var csState = require('../../../common/models/TypeRegistry').item('CSState')
 module.exports = function(user, message, callback){
     //response [系统]:您的当前状态为-在线-会话中
+    console.log('in vs---------------------------')
     co(function* (){
-        var stat = yield cskv.loadCSStatusByCSOpenId(user.wx_openid);
+        var stat = yield cskv.loadCSStatusByCSOpenIdAsync(user.wx_openid);
         yield wechatApi.sendTextAsync(user.wx_openid, '[系统]:您的当前状态为'+csState.values(user.status));
         callback(null, null)
     })
