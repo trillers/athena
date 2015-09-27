1b7daf159abba6e3d4be604d1979aad7cb22f050
node-rookie <749924911@qq.com>
2015-09-27 18:08:12 +0800

    test

--- src/modules/assistant/handlers/AssistantEventHandler.js
+++ src/modules/assistant/handlers/AssistantEventHandler.js
@@ -2,12 +2,13 @@ var Promise = require('bluebird');
 var co = require('co');
 var logger = require('../../../app/logging').logger;
 var WechatAuthenticator = require('../../user/services/WechatAuthenticator');
-var QrChannelService = require('../')
+var QrChannelService = require('../../qrchannel/services/QrChannelService');
 var authenticator = new WechatAuthenticator({});
 var RoleEmitter = require('../RoleEmitter');
 var roleEmitter = new RoleEmitter();
 var AssistantService = require('../services/AssistantService');
 var wechatApi = require('../../wechat/common/api').api;
+var co = require('co');
 
 require('../../cs/handlers/CsHandler')(roleEmitter);
 require('../../admin/handlers/AdminHandler')(roleEmitter);
@@ -24,31 +25,33 @@ module.exports = function(emitter){
             }
 
             var sceneId = context.weixin.SceneId;
-            try{
-                var success = false, reply = '';
-                var qr = yield QrChannelService.loadBySceneIdAsync(sceneId);
-                if(qr){
-                    switch(qr.type){
-                        case 'cs':
-                            reply = '欢迎成为客服人员！';
-                            success = yield QrChannelService.RegistryCS(user);
-                            break;
-                        //TODO another qr type
-                    }
-                    if(!success){
-                        reply = '系统繁忙，请稍后重试！';
-                    }
-                    wechatApi.sendText(user.wx_openid, reply);
+            co(function*(){
+                try{
+                    var success = false, reply = '';
+                    var qr = yield QrChannelService.loadBySceneIdAsync(sceneId);
+                    if(qr){
+                        switch(qr.type){
+                            case 'cs':
+                                reply = '欢迎成为客服人员！';
+                                success = yield QrChannelService.RegistryCS(user);
+                                break;
+                            //TODO another qr type
+                        }
+                        if(!success){
+                            reply = '系统繁忙，请稍后重试！';
+                        }
+                        wechatApi.sendText(user.wx_openid, reply);
 
+                    }
+                    else{
+                        reply = '该二维码已失效';
+                        wechatApi.sendText(user.wx_openid, reply);
+                    }
+                }catch(err){
+                    logger.err('qr event handler err: ' + err);
+                    return;
                 }
-                else{
-                    reply = '该二维码已失效';
-                    wechatApi.sendText(user.wx_openid, reply);
-                }
-            }catch(err){
-                logger.err('qr event handler err: ' + err);
-                return;
-            }
+            });
         });
     });
 };
