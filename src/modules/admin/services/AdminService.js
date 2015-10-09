9be2c9f8a25ee38e99496c9e88946de204b444c7
henryleu <henryleu@126.com>
2015-10-09 15:00:11 +0800

    add CustomerService, CsService and AdminService

---
+++ src/modules/admin/services/AdminService.js
@@ -0,0 +1,32 @@
+var Promise = require('bluebird');
+var settings = require('athena-settings');
+var User = require('../models/User').model;
+var UserHelper = require('../models/User').helper;
+var UserState = require('../../../framework/model/enums').UserState;
+var UserKv = require('../kvs/User');
+var logger = require('../../../app/logging').logger;
+var u = require('../../../app/util');
+var wechat = require('../../wechat/common/api');
+var cbUtil = require('../../../framework/callback');
+var Service = {};
+
+/**
+ * Create an admin user from openid
+ * @param openid
+ * @param callback
+ */
+Service.createFromOpenid = function(openid, callback){
+
+};
+
+/**
+ * Set user role to admin role by openid
+ * @param openid
+ * @param callback
+ */
+Service.setRoleByOpenid = function(openid, callback){
+
+};
+
+Service = Promise.promisifyAll(Service);
+module.exports = Service;
