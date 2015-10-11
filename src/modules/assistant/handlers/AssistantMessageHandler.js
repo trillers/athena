a5c3bd744716f96ce19f3d5b57c6ad2ab3b374ed
henryleu <henryleu@126.com>
2015-10-11 16:42:47 +0800

    add three setting role commands

--- src/modules/assistant/handlers/AssistantMessageHandler.js
+++ src/modules/assistant/handlers/AssistantMessageHandler.js
@@ -11,8 +11,10 @@ require('../../customer/handlers/CustomerHandler')(roleEmitter);
 
 var CommandRegistry = require('../../../framework/wechat/command-registry');
 var registry = new CommandRegistry();
-registry.addCommand('删除当前用户', require('./commands/deleteUserCommand'))
-
+registry.addCommand('删除当前用户', require('./commands/deleteUserCommand'));
+registry.addCommand('成为客户', require('./commands/setRoleToCustomerCommand'));
+registry.addCommand('成为客服', require('./commands/setRoleToCsCommand'));
+registry.addCommand('成为管理员', require('./commands/setRoleToAdminCommand'));
 
 module.exports = function(emitter){
     emitter.message(function(event, context){
