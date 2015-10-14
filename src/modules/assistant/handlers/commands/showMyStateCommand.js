f7423d0e8e83ee6a8b48d488dd43987bb88c618f
leekangtaqi <leekangtaqi@sina.com>
2015-10-14 17:37:41 +0800

    modify show state cmd

--- src/modules/assistant/handlers/commands/showMyStateCommand.js
+++ src/modules/assistant/handlers/commands/showMyStateCommand.js
@@ -2,6 +2,8 @@ var wechatApi = require('../../../wechat/common/api').api;
 var userRole = require('../../../common/models/TypeRegistry').item('UserRole');
 var csState = require('../../../common/models/TypeRegistry').item('CSState');
 var cskv = require('../../../cs/kvs/CustomerService');
+var cvsKv = require('../../../conversation/kvs/Conversation');
+var userService = require('../../../user/services/UserService');
 var co = require('co');
 module.exports = function(context){
     var message = context.weixin;
@@ -10,14 +12,34 @@ module.exports = function(context){
     co(function* (){
         try {
             var user = yield context.getUser();
-            reply = '[系统]:您的角色为' + userRole.values(user.role);
+            var customer = null;
+            reply = '[系统]: 您的角色为' + userRole.values(user.role);
             if(user.role === userRole.CustomerService.value()){
                 var stat = yield cskv.loadCSStatusByCSOpenIdAsync(message.FromUserName);
+                var cvsId = yield cvsKv.delCurrentCidAsync(user.id);
                 if(typeof stat === 'string'){
-                    reply += ' 当前状态为: ' + csState.values(stat);
+                    reply += ', 当前状态为: ' + csState.values(stat);
                 }
                 else{
-                    reply += ' 当前状态为: ' + csState.offline.title();
+                    reply += ', 当前状态为: ' + csState.offline.title();
+                }
+                if(cvsId){
+                    var cvs = yield cvsKv.loadByIdAsync(cvsId);
+                    if(!cvs){
+                        throw new Error('show my state error occur');
+                    }
+                    customer = yield userService.loadByIdAsync(cvs.initiator);
+                    reply += ', 与"' + customer.wx_nickname + '"会话中…';
+                }else{
+                    reply += ', 无会话';
+                }
+            }
+            if(user.role == userRole.Customer.value()){
+                var cvsId = yield cvsKv.getCurrentId(user.id);
+                if(cvsId){
+                    reply += ', 会话中';
+                }else{
+                    reply += ', 无回话';
                 }
             }
             yield wechatApi.sendTextAsync(message.FromUserName, reply);
