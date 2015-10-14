4730fa0fddb1ce6751b8a6739546ef8298ff5f25
node-rookie <749924911@qq.com>
2015-10-14 09:41:55 +0800

    debug set role

--- src/modules/assistant/handlers/commands/setRoleToCustomerCommand.js
+++ src/modules/assistant/handlers/commands/setRoleToCustomerCommand.js
@@ -4,7 +4,7 @@ var customerService = require('../../../customer/services/CustomerService');
 
 var commandHandler = function(context){
     var openid = context.weixin.FromUserName;
-    customerService.setRoleByOpenid(openid, function(err){
+    customerService.setRoleByOpenid(openid, function(err, user){
         if(err){
             wechatApi.sendTextAsync(openid, '[系统]: 用户 ['+ (user && user.wx_nickname || openid) +'] 切换客户失败');
         }
