9adc3b19a423e0a72aa5cc34a27c7882032a9930
leekangtaqi <leekangtaqi@sina.com>
2015-10-14 15:03:54 +0800

    fix bug

--- src/modules/cs/handlers/commands/offlineCommand.js
+++ src/modules/cs/handlers/commands/offlineCommand.js
@@ -3,12 +3,18 @@ var wechatApi = require('../../../wechat/common/api').api;
 module.exports = function(emitter){
     emitter.offline(function(context){
         var message = context.weixin;
-        cskv.remWcCSSetAsync(message.FromUserName)
+        context.getUser()
+            .then(function(user){
+                return cskv.remWcCSSetAsync(user.id);
+            })
             .then(function(){
                 return cskv.delCSSByIdAsync(message.FromUserName);
             })
             .then(function(){
                 wechatApi.sendText(message.FromUserName, '[系统]:您已下线', function(err, result){});
-            });
+            })
+            .catch(Error, function(e){
+                console.log(e);
+            })
     })
 };
