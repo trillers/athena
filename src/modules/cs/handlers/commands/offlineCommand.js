31ce1f64977241634f5162a447aaffd7706cc52c
leekangtaqi <leekangtaqi@sina.com>
2015-10-14 15:18:45 +0800

    add offline

--- src/modules/cs/handlers/commands/offlineCommand.js
+++ src/modules/cs/handlers/commands/offlineCommand.js
@@ -1,11 +1,27 @@
 var cskv = require('../../kvs/CustomerService');
+var cvsService = require('../../../conversation/services/ConversationService');
 var wechatApi = require('../../../wechat/common/api').api;
 module.exports = function(emitter){
     emitter.offline(function(context){
         var message = context.weixin;
+        var user = null;
         context.getUser()
-            .then(function(user){
+            .then(function(data){
+                user = data;
                 return cskv.remWcCSSetAsync(user.id);
+            })
+            .then(function(){
+                return cvsService.filterAsync({
+                    conditions:{
+                        csId: user.id
+                    }
+                })
+            })
+            .then(function(cvsArr){
+                if(cvsArr.length>0){
+                    return cvsService.closeAsync(cvsArr[0]);
+                }
+                return;
             })
             .then(function(){
                 return cskv.delCSSByIdAsync(message.FromUserName);
