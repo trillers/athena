f5bc769f4a4ed1c880a7211d5c8c2d1b8906a1d2
henryleu <henryleu@126.com>
2015-10-15 11:35:38 +0800

    refactoring role emitter and customer emitter

--- src/modules/assistant/handlers/AssistantMessageHandler.js
+++ src/modules/assistant/handlers/AssistantMessageHandler.js
@@ -1,8 +1,9 @@
-var RoleEmitter = require('../common/RoleEmitter');
-var roleEmitter = new RoleEmitter();
-require('../../cs/handlers/CsHandler')(roleEmitter);
-require('../../admin/handlers/AdminHandler')(roleEmitter);
-require('../../customer/handlers/CustomerHandler')(roleEmitter);
+var roleEmitter = require('../roleEmitter');
+//var RoleEmitter = require('../common/RoleEmitter');
+//var roleEmitter = new RoleEmitter();
+//require('../../cs/handlers/CsHandler')(roleEmitter);
+//require('../../admin/handlers/AdminHandler')(roleEmitter);
+//require('../../customer/handlers/CustomerHandler')(roleEmitter);
 
 var CommandRegistry = require('../../../framework/wechat/command-registry');
 var registry = new CommandRegistry();
