c63c88f772f2bfa0c0e9d66cd992f90fd7bade45
leekangtaqi <leekangtaqi@sina.com>
2015-10-15 16:31:47 +0800

    add contribution

--- src/modules/assistant/handlers/AssistantEventHandler.js
+++ src/modules/assistant/handlers/AssistantEventHandler.js
@@ -9,7 +9,7 @@ var adminService = require('../../admin/services/AdminService');
 module.exports = function (emitter) {
     var createCustomer = require('./commands/createCustomerCommand');
     emitter.event(function (event, context) {
-        console.log(context.weixin);
+        console.log(context.weixin);    
     });
     emitter.subscribe(function(event, context){
         createCustomer(event, context, function(err){
