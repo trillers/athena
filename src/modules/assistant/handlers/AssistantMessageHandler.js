3e1f5af9d67cfa8ec3c89c97625bae28df537085
henryleu <henryleu@126.com>
2015-10-15 11:39:16 +0800

    -a

--- src/modules/assistant/handlers/AssistantMessageHandler.js
+++ src/modules/assistant/handlers/AssistantMessageHandler.js
@@ -1,10 +1,4 @@
 var roleEmitter = require('../roleEmitter');
-//var RoleEmitter = require('../common/RoleEmitter');
-//var roleEmitter = new RoleEmitter();
-//require('../../cs/handlers/CsHandler')(roleEmitter);
-//require('../../admin/handlers/AdminHandler')(roleEmitter);
-//require('../../customer/handlers/CustomerHandler')(roleEmitter);
-
 var CommandRegistry = require('../../../framework/wechat/command-registry');
 var registry = new CommandRegistry();
 registry.addCommand('删除当前用户', require('./commands/deleteUserCommand'));
