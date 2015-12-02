dfacac03889053be4e7e5f0af52102cc5a545a7c
henryleu <henryleu@126.com>
2015-12-02 17:36:08 +0800

    add interType and interId to Tenant model

--- src/modules/tenant/models/Tenant.js
+++ src/modules/tenant/models/Tenant.js
@@ -1,4 +1,5 @@
 var TenantType = require('../../common/models/TypeRegistry').item('TenantType');
+var IntegrationType = require('../../common/models/TypeRegistry').item('IntegrationType');
 
 var Model = function(domainBuilder){
     var schema = domainBuilder
@@ -9,6 +10,10 @@ var Model = function(domainBuilder){
         .withProperties({
             name:           {type: String, required: true}
             , type:         {type: String, enum: TenantType.valueList(), default: TenantType.Personal.value(), required: true}
+
+            //integration type: internal: 内部租户 | external：外部租户
+            , interType:    {type: String, enum: IntegrationType.valueList(), default: IntegrationType.Internal.value(), required: true}
+            , interId:      {type: String, required: false} //integration external id
             , administrative:      {type: Boolean, default: false}
             , desc:         {type: String}
         })
