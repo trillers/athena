4d7af6af784184df47d6a5eaefa72c5e8a55f32c
henryleu <henryleu@126.com>
2015-12-02 18:34:48 +0800

    add Wechat Medium Kv

--- src/modules/medium/base/services/WechatMediumService.js
+++ src/modules/medium/base/services/WechatMediumService.js
@@ -6,11 +6,21 @@ var Service = function(context){
 };
 
 Service.prototype.create = function(mediumJson, callback){
+    var kv = this.context.kvs.wechatMedium;
     var WechatMedium = this.context.models.WechatMedium;
     var medium = new WechatMedium(mediumJson);
     medium.save(function (err, result, affected) {
-        //TODO: need logging
-        cbUtil.handleAffected(callback, err, result, affected);
+        cbUtil.logCallback(
+            err,
+            'Fail to save wechat medium: ' + err,
+            'Succeed to save wechat medium');
+
+        cbUtil.handleAffected(function(err, doc){
+            var obj = doc.toObject({virtuals: true});
+            kv.saveById(obj, function(err, obj){
+                if(callback) callback(err, obj);
+            });
+        }, err, result, affected);
     });
 
 };
--- src/modules/medium/platform/kvs/index.js
+++ src/modules/medium/platform/kvs/index.js
@@ -1,10 +1,8 @@
 var assert = require('assert');
-var context = require('../../../context');
-var Tenant = require('./Tenant');
+var context = require('../../../../context');
 var Platform = require('./Platform');
 var _exports = {};
 
-_exports.tenant        = new Tenant(context);
 _exports.platform        = new Platform(context);
 
 module.exports = context.kvs = _exports;
