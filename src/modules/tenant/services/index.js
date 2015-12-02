4fb0bb774ab78e536dc413d43f2e301412b3b7c5
henryleu <henryleu@126.com>
2015-12-02 18:09:36 +0800

    create ExternalTenantServcie

--- src/modules/tenant/services/index.js
+++ src/modules/tenant/services/index.js
@@ -2,11 +2,13 @@ var u = require('../../../app/util');
 var context = require('../../../context');
 var TenantService = require('./TenantService');
 var PlatformTenantService = require('./PlatformTenantService');
+var ExternalTenantService = require('./ExternalTenantService');
 var TenantMemberService = require('./TenantMemberService');
 var _exports = {};
 
 _exports.tenantService         = new TenantService(context);
 _exports.platformTenantService = new PlatformTenantService(context);
+_exports.externalTenantService = new ExternalTenantService(context);
 _exports.tenantMemberService   = new TenantMemberService(context);
 
 u.extend(context.services, _exports);
